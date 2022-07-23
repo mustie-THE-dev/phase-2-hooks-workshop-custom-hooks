24d40610bd5fa9eb40c6c05422e123f4a03cddb2
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-23 16:44:03 +0300

    Done.

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,22 +1,22 @@
+
+
 import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 
-/* ✅ modify this usePokemon custom hook to take in a query as an argument */
-export function usePokemon() {
-  /* ✅ this hook should only return one thing: an object with the pokemon data */
-}
-
-function Pokemon({ query }) {
-  /* 
-   ✅ move the code from the useState and useEffect hooks into the usePokemon hook
-   then, call the usePokemon hook to access the pokemon data in this component
-  */
+export function usePokemon(query) {
   const [pokemon, setPokemon] = useState(null);
+
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
       .then(r => r.json())
       .then(setPokemon);
   }, [query]);
+
+  return { data: pokemon };
+}
+
+function Pokemon({ query }) {
+  const { data: pokemon } = usePokemon(query);
 
   // 🚫 don't worry about the code below here, you shouldn't have to touch it
   if (!pokemon) return <h3>Loading...</h3>;
@@ -51,29 +51,3 @@ export default function App() {
     </Wrapper>
   );
 }
-
-const Wrapper = styled.section`
-  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.15);
-  display: grid;
-  place-items: center;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  background: papayawhip;
-  text-align: center;
-
-  h1 {
-    background: #ef5350;
-    color: white;
-    display: block;
-    margin: 0;
-    padding: 1rem;
-    color: white;
-    font-size: 2rem;
-  }
-
-  form {
-    display: grid;
-    grid-template-columns: 1fr auto;
-    width: 100%;
-  }
-`;
